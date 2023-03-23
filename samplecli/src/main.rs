909a89bfb9e6902e274cb8264cf06adc6761c721
Shota <riodeja.5.shota@gmail.com>
2023-03-23 22:52:12 +0900

    test: sample test

--- samplecli/src/main.rs
+++ samplecli/src/main.rs
@@ -88,3 +88,13 @@ fn run<R: BufRead>(reader: R, verbose: bool) {
         println!("{}", answer);
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_ok() {
+        assert_eq!(2 * 2, 4);
+    }
+}
