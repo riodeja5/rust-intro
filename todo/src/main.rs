1cedee4c53199de9755d17152d7cb2ebadb92ca1
Shota <riodeja.5.shota@gmail.com>
2023-04-03 23:06:39 +0900

    feat: Hello Wrold

--- todo/src/main.rs
+++ todo/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer};
+
+#[get("/")]
+async fn index() -> Result<HttpResponse, actix_web::Error> {
+    let response_body = "Hello world!";
+
+    // HttpResponse::Ok() はステータスコード200を持つHttpResponseBUilderという構造体を返します。
+    // HttpResponseBuilderのbody()という関数にレスポンスのボディを渡すとHttpResponseが返ってきます。
+    // 戻り値の型がResultなのでOkで包みます。
+    Ok(HttpResponse::Ok().body(response_body))
+}
+
+#[actix_rt::main]
+async fn main() -> Result<(), actix_web::Error> {
+    HttpServer::new(move || App::new().service(index))
+        .bind("0.0.0.0:8080")?
+        .run()
+        .await?;
+    Ok(())
 }
